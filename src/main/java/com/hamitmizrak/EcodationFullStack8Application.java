98bcc927475c1b58ed0e21a8b31d7c2f98af68c7
hamitmizrak <hamitmizrak@gmail.com>
2023-04-01 18:07:03 +0300

    Javase

---
+++ src/main/java/com/hamitmizrak/EcodationFullStack8Application.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcodationFullStack8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcodationFullStack8Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/hamitmizrak/javase/_01_WhatIsJava.java
@@ -0,0 +1,20 @@
+package com.hamitmizrak.javase;
+
+public class _01_WhatIsJava {
+    //single comment
+    /*
+    multiple comment
+    */
+    //IDE - Editor fark ?
+    // compiler ile interpreter nedir ? aralarındaki farklar
+    //API nedir ?
+    // JDK , JRE ,JVM ?
+
+    //psvm
+    public static void main(String[] args) {
+        //değişkenler
+        String $_degiskenAdi44;
+        //sayılarla başlanmaz $_ haricinde =>  özel simgelerle başlanmaz.
+        //Datatypes
+    }
+}
