dea6ba20528a567c628ca18a6d79a19df12785f7
hamitmizrak <hamitmizrak@gmail.com>
2023-04-02 15:55:50 +0300

    Javase Scanner

--- src/main/java/com/hamitmizrak/javase/_02_Examples.java
+++ src/main/java/com/hamitmizrak/javase/_02_Examples.java
@@ -1,5 +1,7 @@
 package com.hamitmizrak.javase;
 
+import java.util.Scanner;
+
 public class _02_Examples {
     public static void main(String[] args) {
         // 1.soru
@@ -8,17 +10,38 @@ public class _02_Examples {
         // x=5;
         // k=3
         // y=?
+        int x=5;
+        int k=3;
+        int y;
+        y=3*x+4*k;
+        System.out.println(y);
 
         // 2.soru
         // Dereceyi Fahrenhayt
         // (derece*9/5)+32
         // 0 derece 32 fahrenhayttır
+        double degree,fahranhayt;
+        degree=0;
+        fahranhayt=(degree*9/5)+32;
+        System.out.println(fahranhayt);
 
         // 3.soru iki sayıda dört işlem
         // x1=4,x2=2;
         // bölme/çıkarma/çarpma/toplama/bölümünden kalan(%)
 
+        //SCANNER
+        Scanner klavye=new Scanner(System.in);
+        int number1,number2;
+        System.out.println("Lütfen bir sayı giriniz");
+        number1=klavye.nextInt();
+        System.out.println("Lütfen ikinci sayı giriniz");
+        number2=klavye.nextInt();
 
+        System.out.println("Topla => "+(number1+number2));
+        System.out.println("Çıkarma => "+((number1-number2)));
+        System.out.println("Çarpma => "+((number1*number2)));
+        System.out.println("Bölme => "+((number1/number2)));
+        System.out.println("Bölümenden kalan => "+((number1%number2)));
 
     }
 }
