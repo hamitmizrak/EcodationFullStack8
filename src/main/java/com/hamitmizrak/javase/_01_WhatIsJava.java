a6be102394d7783af64044a3ab714d7f5800e3be
hamitmizrak <hamitmizrak@gmail.com>
2023-04-02 15:19:52 +0300

    primitive type

--- src/main/java/com/hamitmizrak/javase/_01_WhatIsJava.java
+++ src/main/java/com/hamitmizrak/javase/_01_WhatIsJava.java
@@ -5,9 +5,9 @@ public class _01_WhatIsJava {
     /*
     multiple comment
     */
-    //IDE - Editor fark ?
+    // IDE - Editor fark ?
     // compiler ile interpreter nedir ? aralarındaki farklar
-    //API nedir ?
+    // API nedir ?
     // JDK , JRE ,JVM ?
 
     //psvm
@@ -15,6 +15,25 @@ public class _01_WhatIsJava {
         //değişkenler
         String $_degiskenAdi44;
         //sayılarla başlanmaz $_ haricinde =>  özel simgelerle başlanmaz.
+
         //Datatypes
+        //Tamsayılar
+         byte b1=10;
+        //sout =>TAB
+        short s1=128;
+        int i1=2121212122;
+        long l1=21212121221L;
+        System.out.println(l1);
+
+        //virgüllü sayı
+        float f2=14.5656f;
+        double d2=14141414.454545;
+
+        //Boolean
+        boolean b3=true;
+
+        //Character
+        char c4='@';
+        System.out.println(c4);
     }
 }
