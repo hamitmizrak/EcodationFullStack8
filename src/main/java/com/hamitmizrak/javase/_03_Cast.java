a3aa5b5fc4d69bda2e22759142a74248f483baa8
hamitmizrak <hamitmizrak@gmail.com>
2023-04-02 17:14:02 +0300

    Javase Cast String

--- src/main/java/com/hamitmizrak/javase/_03_Cast.java
+++ src/main/java/com/hamitmizrak/javase/_03_Cast.java
@@ -2,6 +2,10 @@ package com.hamitmizrak.javase;
 
 public class _03_Cast {
     public static void main(String[] args) {
+        // Primitive Type ile Wrapper arasındaki fark ?
+        // Heap hafıza ile Stack arasındaki farklar nelerdir ?
+        // Integer.parseInt() ile Integer.valueOf() arasındaki fark ?
+
         //cast: dönüştürmek:
         //1-) cast
         float f1=14.56f;
@@ -16,7 +20,19 @@ public class _03_Cast {
         Integer wrapper=primitive;
 
         //  String => SAYI
+        String kelime1="14";
+        int sayi1=Integer.parseInt(kelime1);
+        int sayi2=Integer.valueOf(kelime1);
+        System.out.println(sayi1+16);
+        System.out.println(sayi2+16);
+        System.out.println();
+
         //  SAYI   => String
+        int sayi3=25;
+        String kelime3=String.valueOf(sayi3);
+        String kelime4=Integer.toString(sayi3);
+        System.out.println(kelime3+15);
+        System.out.println(kelime4+15);
 
     }
 }
