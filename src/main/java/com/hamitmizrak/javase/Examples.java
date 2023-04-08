c23f7fe50404dc48f42ba8b5ffe49d5568a13dd1
hamitmizrak <hamitmizrak@gmail.com>
2023-04-08 17:15:38 +0300

    Date Examples

---
+++ src/main/java/com/hamitmizrak/javase/Examples.java
@@ -0,0 +1,62 @@
+package com.hamitmizrak.javase;
+
+public class Examples {
+
+    // AKLINDA BİR SAYI
+    // Bir sayi söylüyoruz ve çıkan sonucu tahmin ediyoruz.
+    // ipucu: sonuc her zaman 11 cıkıyor.
+    // kullanıcıdan alınan sayının sonucu tahmin etmek
+    // adım1: 1-100 arasında bir sayı olsun
+    // adım1: Bu sayiyi 2 ile çarpın
+    // adım2: sonuca 22 ekleyin// adım3: cikan sonucu 2 ile bolelim
+    // adım4: bu sayiyi ilk sayidan çıkarın
+
+    // Örnek: 1-10 arasında 5 tane random sayı üretelim ve bunları toplayalım?
+    // initial data sıfır toplamada etkisiz eleman
+
+
+    // Kullanıcı tarafından girilen haftanın gününü bulan algoritma
+    // 1=pazartesi 2=salı ... 7=Pazar (switch-case)
+
+
+    //ÖDEV
+    // Vize Final Ödevi
+    // kullanıcıdan alınan vize ve final notuna göre geçme(ortalama) Algoritması
+    // eğer kullanıcı vize veya final 0(sıfır) bir değer girerse sistemden atsın
+    // ekranda "Sifir disiplin hatasi Sistemden cikiliyor"
+    // bunun haricinde sürekli vize final sorsun
+    // Not Harfleri ==> AA BA BB FF
+    // not ortalaması: ortalama<50 altında ise kaldı FF
+    // not ortalaması: ortalama==50 Geçti
+    // not ortalaması: 55<=x<=69 BB
+    // not ortalaması: 70<=x<=84 BA
+    // not ortalaması: 85<=x<=100 AA
+    // Vize:40% Final:60%
+    // Dikkat: Consolda virgül kullanmalısın.
+    // while sonsuz döngü   ==> while(true) {}
+    // for sonsuz döngü     ==> for(;;) {}
+
+    //ÖDEV
+    // kullanıcıdan aldığımız isim soyisim (boşluk var)
+    // ilk karakter göstersin sonraki kelimeleri masking (maskeleme yapsın)
+    // eğer kullanıcı isim ve soyisimi ilk karakteri küçük girmişse büyük olsun mutlaka
+    // Hamit Mızrak// H**** MIZ***
+    // ipucu: charAt, substring, indexOf, döngü, karar mekanizma
+
+
+    // Örnek-3 (anlamak)
+    // girilen karakter harf mi , sayı mı , özel simge mi ?
+    // Character.isDigit => SAYI
+    // Character.isLetter => HARF
+
+
+    // ÖDEV
+    // Kullanıcı tarafından alınan bir sayıya göre ceza kesilecektir.
+    // Hızı:79 kadar ise para cezası  : 0 TL      ==>  1<=speed<=79
+    // Hızı:80 ise para cezası : 800 TL hızlısınız   ==>  80<=speed<=94
+    // Hızı:95 ise para cezası : 1200 TL çok hızlısınız   ==>  95<=speed<=119//
+    // Hızı:120 ise para cezası: 2000 TL  tehlikeli hızlısınız ==>  speed>=120//
+    // Soför kaç kere trafik cezası yediğini ve toplam para cezasını//
+    // ve eğer toplam ceza sayısı 5 ve üstüne ise ehliyetine el konulsun  hesaplayan algoritma yazalım ?
+
+}
