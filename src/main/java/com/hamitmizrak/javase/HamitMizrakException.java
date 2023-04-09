ea8f3f30d8f487059c7ed4c6d7f74943ed37b94c
hamitmizrak <hamitmizrak@gmail.com>
2023-04-09 15:26:10 +0300

    Exception

---
+++ src/main/java/com/hamitmizrak/javase/HamitMizrakException.java
@@ -0,0 +1,7 @@
+package com.hamitmizrak.javase;
+
+public class HamitMizrakException extends Exception{
+    public HamitMizrakException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/hamitmizrak/javase/_10_Exception.java
+++ src/main/java/com/hamitmizrak/javase/_10_Exception.java
@@ -1,20 +1,27 @@
 package com.hamitmizrak.javase;
 
+import java.io.IOException;
+
 public class _10_Exception {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, IndexOutOfBoundsException, ArithmeticException, HamitMizrakException {
         // Exception Handling
-        // Access Modifier
-        // Encapsulation
-        // Overloading
-        // Override
-        // Method
-        // UUID
-        // Recursive
-        // Faktöriyel(iterative, recursive)
-        // Enum
-        // Diziler
-        // inner class
-        // File
+        System.out.println("1.alan");
+        try {
+            int number = 4 / 0;
+            System.out.println(number);
+        } catch (ArithmeticException ai) {
+            System.out.println("İstisna: " + ai);
+            ai.printStackTrace();
+        } catch (Exception exception) {
+            System.out.println("İstisna: " + exception);
+        } finally {
+            System.out.println("db.close");
+        }
+        System.out.println("2.alan");
+
+        // throw  new IndexOutOfBoundsException("Zorunlu istisnalar meydana getiririz.");
+        // throw  new HamitMizrakException("Special Exception Zorunlu istisnalar meydana getiririz.");
+
     }
 }
