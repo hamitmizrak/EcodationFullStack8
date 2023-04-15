d22705e658c078a4e1c3cb10a9d508769a78b928
hamitmizrak <hamitmizrak@gmail.com>
2023-04-15 17:54:42 +0300

    File

--- src/main/java/com/hamitmizrak/javase/_18_File.java
+++ src/main/java/com/hamitmizrak/javase/_18_File.java
@@ -1,9 +1,31 @@
 package com.hamitmizrak.javase;
+
+import java.io.File;
+
 // File
-// OOP (class,interface,inheritabce,abstract)
+// Thread
 // Generics
 // Collection
+// OOP (class,interface,inheritance,abstract)
 // Java8 (Stream API)
-// Thread
 public class _18_File {
+    public static void main(String[] args) {
+        String URL="C:\\io\\ecodation\\eco8.txt";
+        File file=new File(URL);
+        System.out.println("Dosya mısın "+file.isFile());
+        System.out.println("Dizin misin "+file.isDirectory());
+        System.out.println("canExecute "+file.canExecute());
+        System.out.println("canWrite "+file.canWrite());
+        System.out.println("canRead "+file.canRead());
+        System.out.println("getAbsolutePath:  "+file.getAbsolutePath());
+        System.out.println("Path:  "+file.getPath());
+        System.out.println("getParentFile:  "+file.getParentFile());
+        System.out.println("getName:  "+file.getName());
+        System.out.println("getFreeSpace:  "+file.getFreeSpace());
+        System.out.println("getTotalSpace:  "+file.getTotalSpace());
+
+        //Dosya yazmak dosya okumak
+        //FileWriter BufferedWriter
+        //FileReader BufferedReader
+    }
 }
