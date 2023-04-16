d6d4460f9f70701e4d4b3ffdaede5fa53bdf58b0
hamitmizrak <hamitmizrak@gmail.com>
2023-04-16 17:40:39 +0300

    Threading-1 -2 -3

--- src/main/java/com/hamitmizrak/javase/_19_Threading3.java
+++ src/main/java/com/hamitmizrak/javase/_19_Threading3.java
@@ -1,4 +1,21 @@
 package com.hamitmizrak.javase;
 
 public class _19_Threading3 {
+
+    public static void main(String[] args) {
+        Runnable runnable=new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 10; i++) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println(i+" ");
+                }
+            }
+        };
+        runnable.run();
+    }
 }
