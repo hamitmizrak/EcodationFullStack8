911f36b6c817a42dc4b063b700fbed99a6ed5cca
hamitmizrak <hamitmizrak@gmail.com>
2023-04-30 15:39:36 +0300

    pom.xml, application.properties,messages.properties, ValidationMessages.properties

--- src/main/java/com/hamitmizrak/EcodationFullStack8Application.java
+++ src/main/java/com/hamitmizrak/EcodationFullStack8Application.java
@@ -1,13 +1,51 @@
 package com.hamitmizrak;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
+
+// auditorAware icin
+// @EnableJpaAuditing(auditorAwareRef = "auditorAwareMethod")
+
+//SCAN
+// @EntityScan(basePackages = "com.hamitmizrak.data.entity") //Entity bulamadığı zaman
+// @EnableJpaRepositories(basePackages ="com.hamitmizrak.data.repository" ) //Repository bulamadığı zaman
+
+// Spring Security
+/*
+@SpringBootApplication(exclude = {
+        //SecurityAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+}
+)
+*/
 
 @SpringBootApplication
 public class EcodationFullStack8Application {
-
-    public static void main(String[] args) {
-        SpringApplication.run(EcodationFullStack8Application.class, args);
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("IST"));
     }
 
-}
+    // PSVM
+    public static void main(String[] args) {
+        // devtools active isActive
+         System.setProperty("spring.devtools.restart.enabled","true");
+
+
+        // PORT Ayarlamak
+        /*
+        SpringApplication app = new SpringApplication(TurgutUniversitySpringAllInOneApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8083"));
+        app.run(args);
+         */
+
+        //Disables headless JOptionPane
+        System.setProperty("java.awt.headless", "false");
+
+        SpringApplication.run(EcodationFullStack8Application.class, args);
+    } // end PSVM
+}// end class
