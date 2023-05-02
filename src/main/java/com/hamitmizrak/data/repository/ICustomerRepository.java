0ccdb5cedbf69507d58cfe11f10121edd4bb0813
hamitmizrak <hamitmizrak@gmail.com>
2023-05-02 17:50:05 +0300

    ICustomerRepository

---
+++ src/main/java/com/hamitmizrak/data/repository/ICustomerRepository.java
@@ -0,0 +1,11 @@
+package com.hamitmizrak.data.repository;
+
+import com.hamitmizrak.data.entity.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends JpaRepository<CustomerEntity,Long> {
+
+    //delivered Query
+}
