4888a222470607f2e048eb13ab4a86ac4d93cbdb
hamitmizrak <hamitmizrak@gmail.com>
2023-05-04 09:02:38 +0300

    Caching

--- src/main/java/com/hamitmizrak/EcodationFullStack8Application.java
+++ src/main/java/com/hamitmizrak/EcodationFullStack8Application.java
@@ -3,8 +3,12 @@ package com.hamitmizrak;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import java.util.TimeZone;
+
+// Spring Cache aktif etmek gerekiyor
+@EnableCaching
 
 // auditorAware icin
  @EnableJpaAuditing(auditorAwareRef = "auditorAwareBeanMethod")
---
+++ src/main/java/com/hamitmizrak/controller/api/CachingTutorials.java
@@ -0,0 +1,23 @@
+package com.hamitmizrak.controller.api;
+
+import com.hamitmizrak.business.dto.CustomerDto;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+public class CachingTutorials {
+
+
+    // LIST
+    // http://localhost:2222/customer/api/v1/list
+   /*
+   @Override
+    @GetMapping( "list")
+    @Cacheable(value = "cacheCustomerList")
+    public ResponseEntity<List<CustomerDto>> getAllCustomers() {
+        return ResponseEntity.ok(customerServices.getAllCustomers());
+    }
+    */
+}
