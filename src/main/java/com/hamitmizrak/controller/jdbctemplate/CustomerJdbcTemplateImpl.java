692809ad343d01fd3604b20bacfc006d571ac1c4
hamitmizrak <hamitmizrak@gmail.com>
2023-05-04 11:03:14 +0300

    Spring JDBC Template NamedParameterJdbcTemplate

--- src/main/java/com/hamitmizrak/controller/jdbctemplate/CustomerJdbcTemplateImpl.java
+++ src/main/java/com/hamitmizrak/controller/jdbctemplate/CustomerJdbcTemplateImpl.java
@@ -3,6 +3,10 @@ package com.hamitmizrak.controller.jdbctemplate;
 import com.hamitmizrak.business.dto.CustomerDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
@@ -11,10 +15,14 @@ import java.util.Optional;
 @RequiredArgsConstructor //injection
 
 // JDBC Template
+@Repository
 public class CustomerJdbcTemplateImpl implements ICustomerJdbcTemplate {
 
-    //injection
-    private final JdbcTemplate jdbcTemplate;
+    // INJECTION
+    // Classic template
+    private final JdbcTemplate jdbcTemplate; // klasik SQL için
+    // NamedParameterJdbcTemplate, klasik yer tutucu yerine adlandırılmış parametreler için destek ekler
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     // -- CREATE SCHEMA `spring` DEFAULT CHARACTER SET utf8 ;
     // use spring;
@@ -31,7 +39,7 @@ public class CustomerJdbcTemplateImpl implements ICustomerJdbcTemplate {
 
 
     // SQL ==> SELECT * FROM spring.customer;
-    //LIST
+    // LIST
     @Override
     public List<CustomerDto> findAll() {
         return jdbcTemplate.query(
@@ -48,16 +56,25 @@ public class CustomerJdbcTemplateImpl implements ICustomerJdbcTemplate {
     }
 
 
-
-
-    //UPDATE
+    // UPDATE Classic jdbcTemplate
     // SQL ==> update  spring.customer  set name="Hamit55",surname="Mızrak55" where id =1;
+    /*
     @Override
     @Transactional // create,update,delete için kullan
     public int update(CustomerDto customerDto) {
         return jdbcTemplate.update(
                 "update  spring.customer  set name=?,surname=? where id =?",
                 customerDto.getName(), customerDto.getSurname(), customerDto.getId());
+    }
+    */
+
+    // UPDATE Parameter namedParameterJdbcTemplate
+    @Override
+    @Transactional // create,update,delete için kullan
+    public int update(CustomerDto customerDto) {
+        return namedParameterJdbcTemplate.update(
+                "update  spring.customer  set name=:name,surname=:surname where id =:id",
+                new BeanPropertySqlParameterSource(customerDto));
     }
 
     // DELETE BYID
@@ -78,13 +95,33 @@ public class CustomerJdbcTemplateImpl implements ICustomerJdbcTemplate {
     }
 
 
-    // FIND ID
+    // FIND ID  Classic jdbcTemplate
     // SQL ==> select * from spring.customer where id =9;
+    /*
     @Override
     public Optional<CustomerDto> findById(Long id) {
         return jdbcTemplate.queryForObject(
                 "select * from spring.customer where id = ?",
                 new Object[]{id},
+                // lambda expression
+                (rs, rowNum) ->
+                        Optional.of(new CustomerDto(
+                                rs.getLong("id"),
+                                rs.getString("name"),
+                                rs.getString("surname"), //getBigDecimal
+                                rs.getString("email"),
+                                rs.getString("password")
+                        ))
+        );
+    }
+    */
+
+    // FIND ID  Parameter namedParameterJdbcTemplate
+    @Override
+    public Optional<CustomerDto> findById(Long id) {
+        return namedParameterJdbcTemplate.queryForObject(
+                "select * from spring.customer where id = :id",
+                new MapSqlParameterSource("id", id),
                 // lambda expression
                 (rs, rowNum) ->
                         Optional.of(new CustomerDto(
@@ -130,11 +167,34 @@ public class CustomerJdbcTemplateImpl implements ICustomerJdbcTemplate {
     -- name ve surname
     select * from spring.customer where name like 'h%' or surname like 'm%'
      */
+
+    // LIKE =>  Classic jdbcTemplate
+    /*
     @Override
     public List<CustomerDto> findByNameAndSurname(String name, String surname) {
         return jdbcTemplate.query(
                 "select * from spring.customer where name like 'h%' or surname like 'm%'",
                 new Object[]{"%" + name + "%", surname},
+                (rs, rowNum) ->
+                        new CustomerDto(
+                                rs.getLong("id"),
+                                rs.getString("name"),
+                                rs.getString("surname")
+                        )
+        );
+    }
+     */
+
+    // LIKE =>  Parameter namedParameterJdbcTemplate
+    @Override
+    public List<CustomerDto> findByNameAndSurname(String name, String surname) {
+        MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+        mapSqlParameterSource.addValue("name", "%" + name + "%");
+        mapSqlParameterSource.addValue("surname", surname);
+
+        return namedParameterJdbcTemplate.query(
+                "select * from spring.customer where name like:name or surname like:surname",
+                mapSqlParameterSource,
                 (rs, rowNum) ->
                         new CustomerDto(
                                 rs.getLong("id"),
