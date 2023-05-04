5e9f19b8baacf0662c1a05f9e8a5f7ba343ce016
hamitmizrak <hamitmizrak@gmail.com>
2023-05-04 18:06:24 +0300

    CustomerApi

---
+++ src/main/java/com/hamitmizrak/controller/api/CustomerApiImpl.java
@@ -0,0 +1,84 @@
+package com.hamitmizrak.controller.api;
+
+import com.hamitmizrak.business.dto.CustomerDto;
+import com.hamitmizrak.business.services.ICustomerServices;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+// LOMBOK
+@RequiredArgsConstructor // constructor injection
+
+// REST
+@RestController
+@RequestMapping("/customer/api/v1/")
+@CrossOrigin //CORS
+// Dış dünyaya açılan kapı
+public class CustomerApiImpl implements ICustomerApi {
+
+    // injection
+    private final ICustomerServices customerServices;
+
+    // SPEED DATA
+    @Override
+    public ResponseEntity<List<CustomerDto>> speedData() {
+        return ResponseEntity.ok(customerServices.speedData());
+    }
+
+    // DELETE ALL
+    @Override
+    public ResponseEntity<?> deleteAll() {
+        customerServices.deleteAll();
+        return ResponseEntity.ok("Bütün datalar silindi.");
+    }
+
+    // CREATE POST
+    // http://localhost:2222/customer/api/create/v1
+    @Override
+    @PostMapping("/create/v1")
+    @Transactional // create, delete,update
+    public ResponseEntity<CustomerDto> customerCreatePost(@Valid @RequestBody CustomerDto customerDto) {
+        return ResponseEntity.ok(customerServices.customerCreate(customerDto));
+    }
+
+    // LIST
+    // http://localhost:2222/customer/api/list/v1
+    @Override
+    @GetMapping("/list")
+    @Cacheable(value = "customerListCacheable")
+    public ResponseEntity<List<CustomerDto>> customerList() {
+        return ResponseEntity.ok(customerServices.customerList());
+    }
+
+    // FIND
+    // http://localhost:2222/customer/api/v1/find/1
+    @Override
+    @GetMapping("/find/{id}")
+    @Cacheable(value = "customerFindByIdCacheable")
+    public ResponseEntity<?> customerFindGetById(@PathVariable(name="id") Long id) {
+        // return ResponseEntity.status(200).body(customerServices.customerFind(id));
+        return ResponseEntity.ok(customerServices.customerFind(id));
+    }
+
+    // DELETE
+    // http://localhost:2222/customer/api/v1/delete/1
+    @Override
+    @DeleteMapping("/delete/{id}")
+    @Transactional // create, delete,update
+    public ResponseEntity<CustomerDto> customerDelete(@PathVariable(name="id") Long id) {
+        return ResponseEntity.ok(customerServices.customerDelete(id));
+    }
+
+    // UPDATE POST
+    // http://localhost:2222/customer/api/v1/delete/1
+    @Override
+    @PutMapping("/update/{id}")
+    @Transactional // create, delete,update
+    public ResponseEntity<CustomerDto> customerUpdatePost(@PathVariable(name="id") Long id,@Valid @RequestBody CustomerDto customerDto) {
+        return ResponseEntity.ok(customerServices.customerUpdate(id,customerDto));
+    }
+}
--- src/main/java/com/hamitmizrak/controller/api/ICustomerApi.java
+++ src/main/java/com/hamitmizrak/controller/api/ICustomerApi.java
@@ -8,30 +8,30 @@ import java.util.List;
 public interface ICustomerApi {
 
     // SPEED DATA
-    // http://localhost:2222/customer/api/speed
+    // http://localhost:2222/customer/api/v1/speed
     ResponseEntity<List<CustomerDto>> speedData();
 
     // DELETE ALL
-    // http://localhost:2222/customer/api/deleteAll
-    ResponseEntity<List<CustomerDto>> deleteAll();
+    // http://localhost:2222/customer/api/v1/deleteAll
+    ResponseEntity<?> deleteAll();
 
     // CREATE POST
-    // http://localhost:2222/customer/api/create
+    // http://localhost:2222/customer/api/v1/create
     ResponseEntity<CustomerDto> customerCreatePost(CustomerDto customerDto);
 
     // LIST
-    // http://localhost:2222/customer/api/list
+    // http://localhost:2222/customer/api/v1/list
     ResponseEntity<List<CustomerDto>> customerList();
 
     // FIND
-    // http://localhost:2222/customer/api/find/1
+    // http://localhost:2222/customer/api/v1/find/1
     ResponseEntity<?> customerFindGetById(Long id);
 
     // DELETE
-    // http://localhost:2222/customer/api/delete/1
-    ResponseEntity<CustomerDto> customerDelete(Long id, Model model);
+    // http://localhost:2222/customer/api/v1/delete/1
+    ResponseEntity<CustomerDto> customerDelete(Long id);
 
     // UPDATE POST
-    // http://localhost:2222/customer/api/update/1
+    // http://localhost:2222/customer/api/v1/update/1
     ResponseEntity<CustomerDto> customerUpdatePost(Long id, CustomerDto customerDto);
 }
